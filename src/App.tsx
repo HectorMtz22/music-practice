aec577a2dd9952c461c1cc2640c8b406d6d0a9b5
HectorMtz22 <hec.gre81@gmail.com>
2021-06-06 19:06:01 -0500

    CSS Global Variables

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import { Router } from "@reach/router";
 import { Header } from "components/Header";
 import { GlobalStyles } from "styles/GlobalStyles";
 import { Principal } from "views/Principal";
+import { GlobalVariables } from "styles/GlobalVariables";
 function App() {
   return (
     <Fragment>
       <GlobalStyles />
+      <GlobalVariables />
       <Header />
       <Router>
         <Principal path="/" />
